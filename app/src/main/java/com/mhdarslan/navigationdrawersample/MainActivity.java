1cf9bb0488f8444a56569beed33d3d5e2803a79d
MArslan88 <gery_maz_555@hotmail.com>
2022-10-26 16:10:36 +0500

    Notes Fragment included

--- app/src/main/java/com/mhdarslan/navigationdrawersample/MainActivity.java
+++ app/src/main/java/com/mhdarslan/navigationdrawersample/MainActivity.java
@@ -1,14 +1,91 @@
 package com.mhdarslan.navigationdrawersample;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        // step-1 set toolbar
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,R.string.OpenDrawer,R.string.CloseDrawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // default fragment call on open app
+        loadFragment(new NotesFragment());
+
+        // click listener
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.optNotes:
+                        // notes
+                        loadFragment(new NotesFragment());
+                        break;
+                    case R.id.optHome:
+                        // home
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.settings:
+                        // setting
+                        Toast.makeText(MainActivity.this, "Setting", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                // drawer should be closed automatically after click on any option
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // here we handle the condition if drawer is opened then onBackPress only drawer should be closed
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.container,fragment);
+        ft.commit();
     }
 }
